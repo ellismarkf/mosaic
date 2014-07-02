e9413d0c4f3c991b8288bad8b9bc81d1a9775bd8
Mark Ellis <ellismarkf@gmail.com>
2014-07-02 11:35:42 -0500

    add application file

--- lib/mosaic_app.rb
+++ lib/mosaic_app.rb
@@ -1,5 +1,12 @@
-require_relative 'database.rb'
+require 'pg'
+
+# entities
 require_relative 'user.rb'
 require_relative 'mosaic.rb'
 require_relative 'museum.rb'
+
+#database
+require_relative 'database.rb'
+
+#scripts
 require_relative 'capture_text.rb'
