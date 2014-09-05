deb93cae8ad733ae97b8a7e223d6a756cbe69ec0
Mark Ellis <ellis.mark.f@gmail.com>
2014-09-05 17:47:56 -0500

    hide user and database functionality

--- lib/transliterator.rb
+++ lib/transliterator.rb
@@ -1,21 +1,21 @@
-require 'pg'
+# require 'pg'
 
-# entities
-require_relative 'transliterator/entities/user.rb'
-require_relative 'transliterator/entities/mosaic.rb'
-require_relative 'transliterator/entities/museum.rb'
+# # entities
+# require_relative 'transliterator/entities/user.rb'
+# require_relative 'transliterator/entities/mosaic.rb'
+# require_relative 'transliterator/entities/museum.rb'
 
-#database
-require_relative 'transliterator/databases/environments.rb'
-require_relative 'transliterator/databases/database.rb'
+# #database
+# require_relative 'transliterator/databases/environments.rb'
+# require_relative 'transliterator/databases/database.rb'
 
 #scripts
 require_relative 'transliterator/scripts/capture_text.rb'
-require_relative 'transliterator/scripts/signup.rb'
-require_relative 'transliterator/scripts/sign_in.rb'
+# require_relative 'transliterator/scripts/signup.rb'
+# require_relative 'transliterator/scripts/sign_in.rb'
 require_relative 'transliterator/scripts/color_randomizer.rb'
 
 #assets
 require_relative '../public/js/color_hash.rb'
 
-Transliterator::ORM.new
+# Transliterator::ORM.new
